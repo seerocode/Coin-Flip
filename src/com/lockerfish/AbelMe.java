297a3cc3cca32926d90b83876bf177e04a639a34
abelM01 <assefa.abel@yahoo.com>
2016-12-01 15:22:47 -0500

    java update project

--- src/com/lockerfish/AbelMe.java
+++ src/com/lockerfish/AbelMe.java
@@ -2,6 +2,7 @@ package com.lockerfish;
 
 public class AbelMe {
 	public void main(String[] args){
+		String[] fl = {"Tail", "Tail","Head","Tail","Tail","Head","Head","Tail","Head"};
 		System.out.print("Hello");
 	}
 
