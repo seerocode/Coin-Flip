4b178df1d0b5e9574ba94e51db401558fb5a1b50
seerocode <rosemarie.liriano@gmail.com>
2016-12-05 13:32:40 -0500

    Added CFGame glass to initialize game

---
+++ Coin-Flip/src/coinflip/CFGame.java
@@ -0,0 +1,12 @@
+package coinflip;
+
+import java.net.MalformedURLException;
+
+public class CFGame {
+
+	public static void main(String[] args) throws MalformedURLException {
+		CF newGame = new CF();
+
+	}
+
+}
