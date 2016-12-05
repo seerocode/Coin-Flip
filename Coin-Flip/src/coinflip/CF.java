50e32f5c5aa7ebde0c1a9782bed246083adcb8b7
seerocode <rosemarie.liriano@gmail.com>
2016-12-05 13:25:48 -0500

    Version with clearButton

--- Coin-Flip/src/coinflip/CF.java
+++ Coin-Flip/src/coinflip/CF.java
@@ -14,9 +14,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CF extends JFrame implements ActionListener {
-
-	public static void main(String[] args) throws MalformedURLException {
-
+	
+	JButton clearButton;
+	
+	public CF() throws MalformedURLException {
+		
 		URL url = new URL("file:///Users/alexisflores/Documents/workspace/CoinFlipJFrame/img/coin_flip_cont.gif");
 		Icon icon = new ImageIcon(url);
 		JLabel label = new JLabel(icon);
@@ -41,6 +43,24 @@ public class CF extends JFrame implements ActionListener {
 
 		JLabel headsLabel = new JLabel("[ HEADS = ]");
 		headsLabel.setBounds(300, 70, 250, 50);
+		
+		clearButton = new JButton("Clear");
+		clearButton.setBounds(305, 500, 130, 45);
+		clearButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				window.dispose();
+				try {
+					new CF();
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+			
+		});
 
 		JButton button = new JButton("Flip");
 		button.setBounds(235, 450, 130, 45);
@@ -54,26 +74,85 @@ public class CF extends JFrame implements ActionListener {
 				flipsLabel.setText(Arrays.toString(flip.hAndT));
 				
 				flip.tailsCount();
-				tailsLabel.setText("Total Tails " + Integer.toString(flip.tCount));
+				tailsLabel.setText("Total Tails " + flip.tCount);
 
 				flip.headsCount();
-				headsLabel.setText("Total Tails " + Integer.toString(flip.hCount));
+				headsLabel.setText("Total Tails " + flip.hCount);
 
 			}
 
 		});
 		
 		panel.add(button);
+		panel.add(clearButton);
 		panel.add(flipsLabel);
 		panel.add(tailsLabel);
 		panel.add(headsLabel);
 
 		window.setVisible(true);
 	}
+	
+
+	public static void main(String[] args) throws MalformedURLException {
+
+//		URL url = new URL("file:///Users/alexisflores/Documents/workspace/CoinFlipJFrame/img/coin_flip_cont.gif");
+//		Icon icon = new ImageIcon(url);
+//		JLabel label = new JLabel(icon);
+//
+//		JFrame window = new JFrame("Coin-Flip");
+//		window.getContentPane().add(label);
+//		window.pack();
+//		window.setSize(600, 600);
+//		window.setResizable(false);
+//		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		window.setLocationRelativeTo(null);
+//
+//		JPanel panel = new JPanel();
+//		panel.setLayout(null);
+//		window.add(panel);
+//
+//		JLabel flipsLabel = new JLabel("[ FLIPS = ]");
+//		flipsLabel.setBounds(50, 20, 500, 50);
+//
+//		JLabel tailsLabel = new JLabel("[ TAILS = ]");
+//		tailsLabel.setBounds(50, 70, 250, 50);
+//
+//		JLabel headsLabel = new JLabel("[ HEADS = ]");
+//		headsLabel.setBounds(300, 70, 250, 50);
+//
+//		JButton button = new JButton("Flip");
+//		button.setBounds(235, 450, 130, 45);
+//		button.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//
+//				Flips flip = new Flips();
+//
+//				flipsLabel.setText(Arrays.toString(flip.hAndT));
+//				
+//				flip.tailsCount();
+//				tailsLabel.setText("Total Tails " + flip.tCount);
+//
+//				flip.headsCount();
+//				headsLabel.setText("Total Tails " + flip.hCount);
+//
+//			}
+//
+//		});
+//		
+//		panel.add(button);
+//		panel.add(flipsLabel);
+//		panel.add(tailsLabel);
+//		panel.add(headsLabel);
+//
+//		window.setVisible(true);
+//	}
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+
 
 	}
 }
